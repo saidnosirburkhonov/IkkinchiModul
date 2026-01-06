27bbbc4279e03490eeb10c7d37d6ac5cf09abd1f
Saidnosir Burkhonov <saidnosirburxonov.com>
2026-01-06 13:49:55 +0500

    done

---
+++ Dars2.1/Dars2.1/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+
+namespace Dars2._1;
+
+internal class Product
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public DateTime DateRelease { get; set; }
+    public int ShelfLife { get; set; }
+
+};
+override public string ToString()
+{
+    return $"";
+}
---
+++ Dars2.1/Dars2.1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Dars2._1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Product product1 = new Product()
+        {
+            Name = "Cake",
+            Price = 200000,
+            DateRelease = DateTime.Now,
+            ShelfLife = 7
+        };
+
+        Console.WriteLine($"{product1.DateRelease.Day + product1.ShelfLife}");
+    }
+}
