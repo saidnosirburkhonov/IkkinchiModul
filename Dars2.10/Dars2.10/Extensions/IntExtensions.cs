cf37dff176280a5f77f0c854723c7c47066dfe85
Saidnosir Burkhonov <saidnosirburxonov.com>
2026-01-29 11:17:49 +0500

    done

---
+++ Dars2.10/Dars2.10/Extensions/IntExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dars2._10.Extensions
+{
+    internal class IntExtensions
+    {
+    }
+}
---
+++ Dars2.10/Dars2.10/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Dars2._10;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
